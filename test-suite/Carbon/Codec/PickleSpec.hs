94140f719738c0ccab2d0918dd5ad3165b9d96e2
Maksim Zakharov <m4spam@gmail.com>
2015-10-03 19:02:15 +0300

    Additional tests for Pickle encode/decode

--- test-suite/Carbon/Codec/PickleSpec.hs
+++ test-suite/Carbon/Codec/PickleSpec.hs
@@ -6,8 +6,11 @@ import Carbon
 import Carbon.Codec.Pickle
 
 import qualified Data.ByteString.Lazy as L
+import Data.ByteString (pack)
 
 import Test.Hspec
+import Data.Knob
+import System.IO
 import Carbon.TestExtensions ()
 
 spec :: Spec
@@ -17,3 +20,14 @@ spec = do
         let encoded = encodePickle [metricTuple "metric" 1000 42.0]
         let decoded = decodePickle $ L.toStrict encoded
         decoded `shouldBe` Right [metricTuple "metric" 1000 42.0]
+
+    it "decodes multiple batches from single Handler" $ do
+        knob <- newKnob (pack [])
+        hw <- newFileHandle knob "test.txt" WriteMode
+        writePickled [metricTuple "metric" 1000 42.0, metricTuple "metric" 1010 24.0] hw
+        writePickled [metricTuple "metric" 1020 124.0] hw
+        hClose hw
+
+        hr <- newFileHandle knob "test.txt" ReadMode
+        readPickled hr `shouldReturn` Just [metricTuple "metric" 1000 42.0, metricTuple "metric" 1010 24.0]
+        readPickled hr `shouldReturn` Just [metricTuple "metric" 1020 124.0]
